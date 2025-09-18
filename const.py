1b47f3c932a1b578abd8b766ebff089f99d5db75
Artaiios <42247663+Artaiios@users.noreply.github.com>
2025-09-19 00:19:27 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,7 @@
+DOMAIN = "sizzapp"
+
+CONF_SHARED_CODE = "shared_code"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+DEFAULT_SCAN_INTERVAL = 30  # seconds
+API_URL = "https://api.sizzapp.com/app/location_sharing/info?shared_code={code}"
