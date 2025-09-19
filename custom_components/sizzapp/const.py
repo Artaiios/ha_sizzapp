2fbf8ca8e00fd8e84f9355896d6b607e75264712
Artaiios <42247663+Artaiios@users.noreply.github.com>
2025-09-20 01:27:11 +0200

    Update const.py

--- custom_components/sizzapp/const.py
+++ custom_components/sizzapp/const.py
@@ -1,7 +1,17 @@
 DOMAIN = "sizzapp"
+MANUFACTURER = "Sizzapp"
+
+# Passe diese URL ggf. an dein echtes Share-API-Endpunktformat an:
+API_URL = "https://api.sizzapp.com/app/location_sharing/info?shared_code={code}"
 
 CONF_SHARED_CODE = "shared_code"
-CONF_SCAN_INTERVAL = "scan_interval"
+CONF_SHARE_URL = "share_url"
+CONF_POLL_INTERVAL = "poll_interval"
+CONF_SPEED_UNIT = "speed_unit"       # "kmh" | "mph"
+CONF_COORD_PRECISION = "coord_precision"  # int (0..6)
 
-DEFAULT_SCAN_INTERVAL = 30  # seconds
-API_URL = "https://api.sizzapp.com/app/location_sharing/info?shared_code={code}"
+DEFAULT_POLL_INTERVAL = 60  # Sekunden
+DEFAULT_SPEED_UNIT = "kmh"
+DEFAULT_COORD_PRECISION = 6
+
+PLATFORMS = ["device_tracker", "sensor"]
